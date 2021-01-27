3dba949d8a954d0fe957d714b04522fa6cb0f00e
cavinginrainbows <13323921+cavinginrainbows@users.noreply.github.com>
2021-01-27 14:31:25 -0800

    Adds resources page

--- PythonClub/Club/views.py
+++ PythonClub/Club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index(request):
     return render(request, 'Club/index.html')
+
+def resources(request):
+    resource_list = Resource.objects.all()
+    return render(request, 'Club/resources.html', {'resource_list': resource_list})

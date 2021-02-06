7dd2a4db6d507df83b29aceb2eab908a019fd35e
cavinginrainbows <13323921+cavinginrainbows@users.noreply.github.com>
2021-02-05 17:45:40 -0800

    updates for assignment 6

--- PythonClub/Club/views.py
+++ PythonClub/Club/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
@@ -8,3 +8,11 @@ def index(request):
 def resources(request):
     resource_list = Resource.objects.all()
     return render(request, 'Club/resources.html', {'resource_list': resource_list})
+
+def meetings(request):
+    meetings_list = Meeting.objects.all()
+    return render(request, 'Club/meetings.html', {'meetings_list': meetings_list})
+
+def meetingdetails(request, id):
+    meetinginfo = get_object_or_404(Meeting, pk=id)
+    return render(request, 'Club/meetingdetails.html', {'meetinginfo': meetinginfo})
